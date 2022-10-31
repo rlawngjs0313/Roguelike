ad495e07aff54a0bf0dcbd86aa5304285caacabd
JuHeon <rlawngjs0313@naver.com>
2022-10-31 13:37:14 +0900

    움직임 구현

--- Roguelike.py
+++ Roguelike.py
@@ -1,10 +1,10 @@
-import pygame, sys
+from os import environ
+import pygame
 from datafile import *
 from pygame.locals import *
 
 clock = pygame.time.Clock()
-
-screenSize = (400, 400) # 창 크기
+screenSize = (1000, 1000) # 창 크기
 gameScreen = pygame.display.set_mode(screenSize)    # 창모드
 pygame.display.set_caption('Roguelike!')    # 게임 이름 표시
 pygame.init()
@@ -12,13 +12,41 @@ pygame.init()
 spr_character = SpriteUpscaling('test.png', 16, 16, 8, 8, 11)
 gameScreen.fill("white")
 
-while True:
-    gameScreen.blit(spr_character.spr[0], (174, 174)) # 화면 그리기 (스프라이트, 위치)
+player_x = screenSize[0] / 2
+player_y = screenSize[1] / 2
+dy = 0
+dx = 0
+speed = 10
+
+running = True 
+while running:
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-
-    pygame.display.update() # 화면 업데이트 
+            running = False
+        
+        if event.type == pygame.KEYDOWN: # 움직임 파트
+            if event.key == pygame.K_w:
+                dy -= speed
+            elif event.key == pygame.K_a:
+                dx -= speed
+            elif event.key == pygame.K_s:
+                dy += speed
+            elif event.key == pygame.K_d:
+                dx += speed
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                dy = 0
+            elif event.key == pygame.K_a:
+                dx = 0
+            elif event.key == pygame.K_s:
+                dy = 0
+            elif event.key == pygame.K_d:
+                dx = 0
+        
+    player_x += dx
+    player_y += dy
+    gameScreen.fill((255,255,255))
+    gameScreen.blit(spr_character.spr[0], (player_x, player_y))
+    pygame.display.flip()
     clock.tick(60)
